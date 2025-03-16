dd92335f98f0375a492ce77735db6a72370559e2
wygol <124050566+wygol@users.noreply.github.com>
2025-03-16 10:41:36 +0100

    Create pythonFileRene.py
    Created a python file with information about me

---
+++ pythonFileRene.py
@@ -0,0 +1,22 @@
+def leastFavoriteFood() -> str:
+    return "I typically eat anything and a lot of it.\n---"
+
+
+def recommendation() -> str:
+    return "Book: Homo Destructor - Werner Bätzing. This is a a human-environment book written by a geographer who focuses on the historical advancements of humanity.\n---"
+  
+
+def quote() -> str:
+    return "bark bark bark - a wise dog\n---"
+
+
+def germanWord() -> str:
+    return "Schnackal --> carinthian dialect for hiccup.\n---"
+
+
+
+if __name__ == "__main__":
+    print(leastFavoriteFood())
+    print(recommendation())
+    print(quote())
+    print(germanWord())
