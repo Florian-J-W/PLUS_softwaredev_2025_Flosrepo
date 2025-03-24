617162ef5b7b4c19bda908472ef5897f1079021d
WinklerFlorian <115723157+WinklerFlorian@users.noreply.github.com>
2025-03-24 11:20:54 +0100

    added handson file

---
+++ handson.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+#print(sys.path)
+#help("modules")
+#days_of_the_week = 2
+def say_hello(recipient):
+    print("Hello, world! Hello {} ".format(recipient))
+    return recipient
+
+say_hello('Flo')
